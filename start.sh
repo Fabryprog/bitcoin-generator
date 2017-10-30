b610e477e1093936a0d162ac8147de0f6627c036
FabryProg <fabryprog@gmail.com>
2017-10-30 02:39:17 -0500

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,34 +1,26 @@
 #!/bin/sh
 
-# CRON
-echo "1/5) Adding cron ..."
-
-crontab -u root /root/cron
-cron
-
-echo "1/5) done!"
-
 # START MYSQL
-echo "2/5) Starting MySql Server ..."
+echo "1/4) Starting MySql Server ..."
 
 service mysql start
 
-echo "2/5) done!"
+echo "1/4) done!"
 
 #INIT DATABASE
-echo "3/5) Creating database ..."
+echo "2/4) Creating database ..."
 
 mysqladmin -u root create bitcoin
 
-echo "3/5) done!"
-echo "4/5) Adding rows ..."
+echo "2/4) done!"
+echo "3/4) Adding rows ..."
 
 mysql -u root bitcoin < /root/script.sql
 
-echo "4/5) done!"
+echo "3/4) done!"
 
 # IMPORT USED ADDRESS
-echo "5/5) Adding used address ..."
+echo "4/4) Adding used address ..."
 
 if [ -f "/opt/bitcoin-generator/$ADDRESS" ]; then
     mysql -u root bitcoin < /opt/bitcoin-generator/$ADDRESS
@@ -36,7 +28,7 @@ else
     mysql -u root bitcoin < /root/used_address.sql
 fi
 
-echo "5/5) Init complete!"
+echo "4/4) Init complete!"
 
 for i in `seq $THREADS`; do 
   sh /root/core.sh &
